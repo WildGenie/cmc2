bea1313ccc7248bf33b6cbc3dc55c1a0d2e93713
xla760 <xla760@d83fb555-5f53-0410-b173-fd8f55d8638a>
2008-10-03 13:02:00 +0000

    sw enumeration to datatable + working backgroundworker!!!

--- trunk/CMC/AssemblyInfo.vb
+++ trunk/CMC/AssemblyInfo.vb
@@ -31,7 +31,7 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("2.6.24.0922")> 
+<Assembly: AssemblyVersion("2.6.24.0923")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en-GB")> 
-<Assembly: AssemblyFileVersionAttribute("2.6.24.0922")> 
+<Assembly: AssemblyFileVersionAttribute("2.6.24.0923")> 
